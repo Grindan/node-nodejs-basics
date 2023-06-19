0e43a63c290134e93177f03b98ae557794e490c5
Grindan <polina.koroleva@gmail.com>
2023-06-20 02:01:16 +0300

    feat: add hash task implementation

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,5 +1,17 @@
-const calculateHash = async () => {
-    // Write your code here 
+import fs from 'fs/promises';
+import crypto from 'crypto';
+
+import createPath from '../helpers/createPath.js';
+
+export const calculateHash = async () => {
+    const filePath = createPath(import.meta.url, 'files', 'fileToCalculateHashFor.txt');
+
+    const data = await fs.readFile(filePath);
+
+    const hash = crypto.createHash('sha256').update(data).digest('hex');
+
+    console.log('✅ File hash:', hash);
+    return hash;
 };
 
-await calculateHash();
+calculateHash();
