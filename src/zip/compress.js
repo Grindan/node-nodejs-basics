bfbad34cfc89a56b4fab30a9bce05c679c332495
Grindan <polina.koroleva@gmail.com>
2023-06-20 02:02:47 +0300

    feat: add zip tasks implementation

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,20 @@
-const compress = async () => {
-    // Write your code here 
+import fs from 'fs';
+import zlib from 'zlib';
+import { pipeline } from 'stream/promises';
+
+import createPath from '../helpers/createPath.js';
+
+export const compress = async () => {
+    const filePath = createPath(import.meta.url, 'files', 'fileToCompress.txt');
+    const archivePath = createPath(import.meta.url, 'files', 'archive.gz');
+
+    await pipeline(fs.createReadStream(filePath), zlib.createGzip(), fs.createWriteStream(archivePath))
+        .then(() => {
+            console.log('✅ Compressed.');
+        })
+        .catch((err) => {
+            console.log('❌ Something went wrong:', err);
+        });
 };
 
-await compress();
+compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,20 @@
-const decompress = async () => {
-    // Write your code here 
+import fs from 'fs';
+import zlib from 'zlib';
+import { pipeline } from 'stream/promises';
+
+import createPath from '../helpers/createPath.js';
+
+export const decompress = async () => {
+    const filePath = createPath(import.meta.url, 'files', 'fileToCompress.txt');
+    const archivePath = createPath(import.meta.url, 'files', 'archive.gz');
+
+    await pipeline(fs.createReadStream(archivePath), zlib.createUnzip(), fs.createWriteStream(filePath))
+        .then(() => {
+            console.log('✅ Decompressed.');
+        })
+        .catch((err) => {
+            console.log('❌ Something went wrong:', err);
+        });
 };
 
-await decompress();
+decompress();
